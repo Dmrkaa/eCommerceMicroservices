b54b9dbd1e6b66600c3c9d26187e4899e5092457
Unknow Unknow <dmrkaa@outlook.com>
2022-03-22 20:31:02 +0500

    Added Http methods

--- Ordering/Ordering.API/Controllers/OrderController.cs
+++ Ordering/Ordering.API/Controllers/OrderController.cs
@@ -24,9 +24,41 @@ namespace Ordering.API.Controllers
         public async Task<ActionResult<IEnumerable<OrderDto>>> GetOrdersByUserName(string userName)
         {
             var query = new GetOrderListQuery(userName);
-            var orders = _mediator.Send(query);
+            var orders = await _mediator.Send(query);
+            if (orders.Count == 0)
+            {
+                return NoContent();
+            }
             return Ok(orders);
         }
 
+        [HttpPost(Name = "CheckoutOrder")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<int>> CheckoutOrder([FromBody] CheckOutOrderCommand command)
+        {
+            var result = await _mediator.Send(command);
+            return Ok(result);
+        }
+
+        [HttpPut(Name = "UpdateOrder")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult> UpdateOrder([FromBody] UpdateOrderCommand command)
+        {
+            await _mediator.Send(command);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}", Name = "DeleteOrder")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult> DeleteOrder(int id)
+        {
+            var command = new DeleteOrderCommand() { Id = id };
+            await _mediator.Send(command);
+            return NoContent();
+        }
     }
 }
